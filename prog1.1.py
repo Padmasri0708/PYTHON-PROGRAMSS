fdfcf69a2ab15cac7655e765b9fe52babf8fee03
Padmasri0708 <padmasriadena@gmail.com>
2026-04-27 16:32:38 +0530

    Add files via upload

---
+++ prog1.1.py
@@ -0,0 +1,12 @@
+a=20
+b=5
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+modulo=a%b
+print("The addition is:",add)
+print("The subtraction is:",subtract)
+print("The multiplication is:",mul)
+print("The divison is:",div)
+print("The modulus division is:",modulo)
